ee4c17ae2cb16c131b20cb77486caf9b08d58e53
Garfield Song <songbintaochina@gmail.com>
2016-10-13 18:22:19 +0800

    Create PackageConstant.java

---
+++ src/main/java/garfield/java/jersey/example/common/constant/PackageConstant.java
@@ -0,0 +1,15 @@
+package garfield.java.jersey.example.common.constant;
+
+public final class PackageConstant {
+    
+    public static final String ENTITY_PACKAGE = "garfield.java.jersey.example.entity";
+    
+    public static final String DAO_PACKAGR = "garfield.java.jersey.example.dao";
+    
+    public static final String SERVICE_PACKAGE = "garfield.java.jersey.example.service";
+    
+    public static final String JERSEY_PROVIDER_PACKAGE = "garfield.java.jersey.example.rest.provider";
+    
+    public static final String JERSEY_RESOURCE_PACKAGE = "garfield.java.jersey.example.rest.resource";
+    
+}
